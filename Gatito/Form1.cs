9dfad1226fd24aa4844e4025ca2b44cf095f7e23
JuniorVL10 <joseluisvallejolazcano@gmail.com>
2021-05-13 16:34:35 -0700

    Se completo el Codigo del Ejersicio de Gatito

--- Gatito/Form1.cs
+++ Gatito/Form1.cs
@@ -10,64 +10,172 @@ using System.Windows.Forms;
 
 namespace Gatito
 {
-    public partial class GameGatito : Form
+    public partial class Form1 : Form
     {
         int turno = 1;
         int[,] Gato;
         int PuntosPlayer1 = 0;
-            int PuntosPlayer2 = 0;
+        int PuntosPlayer2 = 0;
         bool YaHayGanador;
 
-
-        public GameGatito()
+        public Form1()
         {
             InitializeComponent();
+            IniciarJuego();
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void pictureBox1_Click_1(object sender, EventArgs e)
+        private void picReiniciar_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void pictureBox3_Click(object sender, EventArgs e)
-        {
+            IniciarJuego();
 
         }
 
         public void IniciarJuego()
         {
-            //iniciar valores en juego
+            //Iniciar Valores en Juego
             turno = 1;
             Gato = new int[3, 3];
             YaHayGanador = false;
 
             picGanador.Image = Properties.Resources.f_0;
             FichasGato.Controls.Clear();
-            //Arreglos para fichsa
+
+            //Arreglos para mostrar las fichas y meter valores en una matriz
             for (var i = 0; i < 3; i++)
             {
                 for (var j = 0; j < 3; j++)
                 {
                     var FichaJuego = new PictureBox();
                     FichaJuego.Image = Properties.Resources.f_0;
-                    FichaJuego.Name = String.Format("{0}", i + "_" + j);
+                    FichaJuego.Name = string.Format("{0}", i + "_" + j);
+                    FichaJuego.Dock = DockStyle.Fill;
                     FichaJuego.Cursor = Cursors.Hand;
                     FichaJuego.SizeMode = PictureBoxSizeMode.StretchImage;
-                        FichasGato.Controls.Add.(FichaJuego, j, i);
+                    FichaJuego.Click += Jugar;
+                    FichasGato.Controls.Add(FichaJuego, j, i);
                     Gato[i, j] = 0;
+
+                }
+            }
+
+        }
+
+        private void Jugar(object sender, EventArgs e)
+        {
+            var FichaSeleccionadaUsuario = (PictureBox)sender;
+            FichaSeleccionadaUsuario.Enabled = false;
+            FichaSeleccionadaUsuario.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("f_" + turno);
+            string[] Posicion = FichaSeleccionadaUsuario.Name.Split("_".ToCharArray());
+            int Fila = Convert.ToInt32(Posicion[0]);
+            int Columna = Convert.ToInt32(Posicion[1]);
+            Gato[Fila, Columna] = turno;
+            verificarJuego(Fila, Columna);
+            turno = (turno == 1) ? 2 : 1;
+        }
+
+        public void verificarJuego(int Fila, int Columna)
+        {
+
+
+            int GanoFilas = 0;
+            int GanoColumnas = 0;
+            int DiagonalPrincipal = 0;
+            int DiagonalInversa = 0;
+            int TamanioGato = 3;
+
+            for (var i = 0; i < TamanioGato; i++)
+            {
+                for (var j = 0; j < TamanioGato; j++)
+                {
+                    if (i == Fila)
+                    {
+                        if (Gato[i, j] == turno)
+                        {
+                            GanoFilas++;
+                        }
+
+                    }
+
+                    if (j == Columna)
+                    {
+                        if (Gato[i, j] == turno)
+                        {
+                            GanoColumnas++;
+                        }
+
+                    }
+
+                    if (i == j) //Diagonal P
+                    {
+                        if (Gato[i, j] == turno)
+                        {
+                            DiagonalPrincipal++;
+                        }
+                    }
+
+                    if ((i + j) == (TamanioGato - 1)) //diagonal i
+                    {
+                        if (Gato[i, j] == turno)
+                        {
+                            DiagonalInversa++;
+                        }
+                    }
+
 
 
 
                 }
-
-
-
             }
+
+            if ((GanoFilas == TamanioGato) || (GanoColumnas == TamanioGato) || (DiagonalInversa == TamanioGato) || (DiagonalPrincipal == TamanioGato))
+            {
+                YaHayGanador = true;
+            }
+
+            //Validar cuando sea empate
+
+            //else
+            //bool Empate =true;
+
+            //for(var i=0; i< TamanioGato; i++)
+            //{ 
+            //for(var j=0; j< TamanioGato; j++)
+            //{
+            // if()
+            //Empate = false;
+            //}
+
+
+            //}
+            //if (Empate)
+
+
+            //{
+            //    MessageBox.Show("Esto es un empate ¡Reinicia el juego!");
+            //    IniciarJuego();
+            //}
+
+            if (YaHayGanador)
+            {
+                MessageBox.Show("Ya hay ganador");
+                picGanador.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("f_" + turno);
+                if (turno == 1)
+                {
+                    PuntosPlayer1++;
+                    lblPlayer1.Text = PuntosPlayer1.ToString();
+                }
+                else
+                {
+                    PuntosPlayer2++;
+                    lblPlayer2.Text = PuntosPlayer2.ToString();
+                }
+            }
+
 
         }
 
